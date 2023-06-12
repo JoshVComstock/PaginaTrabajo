d7270722c1080f734b0e5a52c4b4bbc009c47ee8
JoshVComstock <josedanielveizaga@gmail.com>
2023-06-12 00:15:13 -0400

    styled

---
+++ web/src/components/nav.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+const Nav = () => {
+  return (
+    <>
+      <Container>
+        <img src="" alt="" />
+        <div>
+          <Link>Inicio</Link>
+          <Link>Empleos</Link>
+          <Link>Empresas</Link>
+        </div>
+        <div>
+          <Link>Login</Link>
+          <Link>Registrarse</Link>
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default Nav;
+
+const Container=styled.nav`
+    
+`;
