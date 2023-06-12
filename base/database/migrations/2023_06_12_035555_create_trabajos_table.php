9dcf78f5d50899806b1af880383a4d98b336cb04
TANHEHANZ <hantach10@gmail.com>
2023-06-12 00:14:38 -0400

    back

---
+++ base/database/migrations/2023_06_12_035555_create_trabajos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrabajosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trabajos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->foreignId('id_empresas')->constrained('empresas')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->string('estado');
+            $table->date('fecha');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trabajos');
+    }
+}
